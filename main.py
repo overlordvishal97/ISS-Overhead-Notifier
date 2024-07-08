b5db75dbdbbf78c79fc3c72e5d27bb63c6ee88db
Vishal <83269653+overlordvishal97@users.noreply.github.com>
2024-07-08 23:35:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+MY_LAT = 15.828126 # Your latitude
+MY_LONG = 78.037277 # Your longitude
+my_email = os.getenv("my_email")
+password = os.getenv("password")
+def is_iss_overhead():
+    # api for iss location tracking.
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    # sunrise sunset api to get those for my location.
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=os.getenv("to_addrs"),
+                                msg=f"subject:Look Up \n\nThe Iss is abouve you in the sky.")
